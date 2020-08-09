e00f00e4a2b57a9aa81336e7b69516f10dc9eec0
Crowdbotics <team@crowdbotics.com>
2020-08-09 00:47:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kayo_customs_19459/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kayo_customs_19459;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "kayo_customs_19459";
+    }
+}
